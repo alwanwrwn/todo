8624457fb0004e23140744bc0e14c0d8b943c030
Alwan Wirawan <alwanwrwn@student.uns.ac.id>
2023-09-27 19:39:42 +0700

    add update function & cors

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
     "net/http"
     "encoding/json"
     "github.com/gorilla/mux"
+    "github.com/rs/cors"
 
     _ "github.com/lib/pq"
 )
@@ -35,7 +36,7 @@ func getTodoItems(w http.ResponseWriter, r *http.Request) {
     fmt.Println("get todo")
 
     // Query the database to retrieve all to-do items
-    rows, err := db.Query("SELECT * FROM todo_list")
+    rows, err := db.Query("SELECT * FROM todo_list WHERE completed = FALSE")
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
@@ -93,27 +94,68 @@ func addTodoItem(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusCreated)
 }
 
-func main() { 
+func updateTodoItem(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("update todo")
+
+    var item TodoItem
+
+    err := json.NewDecoder(r.Body).Decode(&item)
+
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusBadRequest)
+      return
+    }
+
+    // Update the to-do item in the database
+    _, err = db.Exec("UPDATE todo_list SET task = $1, completed = $2 WHERE id = $3", item.Task, item.Completed, item.ID)
+    if err != nil {
+      http.Error(w, err.Error(), http.StatusInternalServerError)
+      return
+    }
+    
+    w.WriteHeader(http.StatusOK)
+}
+
+func toDoRouter() http.Handler {
     r := mux.NewRouter()
 
     // Define HTTP routes and handlers
-    http.HandleFunc("/todos", func(w http.ResponseWriter, r *http.Request) {
-        // Implement logic to retrieve and display to-do list items from the database
-        
-        fmt.Println("Daftar todo x")
-        // db.Exec("INSERT INTO todo_list (task, completed) VALUES ('test input', false)")
-        //testPrintge()
-    })
-
     r.HandleFunc("/get-todo", getTodoItems).Methods("GET")
-
+    
     r.HandleFunc("/add-todo", addTodoItem).Methods("POST")
+
+    r.HandleFunc("/update-todo", updateTodoItem).Methods("POST")
+
+    r.Methods(http.MethodOptions).HandlerFunc(handleOptions)
+
+    return r
+}
+
+func handleOptions(w http.ResponseWriter, r *http.Request) {
+    // Set CORS headers to allow the specific origin, methods, and headers
+    w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+    w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+    // Respond with a 200 OK status for the preflight request
+    w.WriteHeader(http.StatusOK)
+}
+
+func main() { 
+    // Create a new CORS middleware instance
+    c := cors.New(cors.Options{
+        AllowedOrigins: []string{"http://localhost:3000"},
+        AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowedHeaders: []string{"Content-Type"},
+        Debug:          true,     
+      })
+
+    http.Handle("/", c.Handler(toDoRouter())) 
 
     // Start the HTTP server
     port := ":8080"
     fmt.Printf("Server is listening on %s...\n", port)
-    if err := http.ListenAndServe(port, r); err != nil {
+    if err := http.ListenAndServe(port, nil); err != nil {
         log.Fatal(err)
     }
 }
-
